f32bfc5ade80f3786231d6bf3e4058bc0b91950a
Krisztian Szep <krisztian.szep@dxc.com>
2020-10-02 14:40:53 +0200

    gitattributes added

---
+++ helloworld.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl 
+  
+# Modules used 
+use strict; 
+use warnings; 
+  
+# Print function  
+print("Hello World\n");
+
+print("$Id$");
+
+print("\nHello World\n");
+
+print("$Format:%ci$ ($Format:%h$");
